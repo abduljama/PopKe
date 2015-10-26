984933487a1282c0ee562d0258b3a0a89baf2d49
abdul <abduljama2009@hotmail.com>
2015-10-26 10:48:40 +0300

    Testing

--- app/src/main/java/com/example/abdul/popeke/Home.java
+++ app/src/main/java/com/example/abdul/popeke/Home.java
@@ -1,16 +1,20 @@
 package com.example.abdul.popeke;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    private Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btn=(Button)findViewById(R.id.btn);
     }
 
     @Override
